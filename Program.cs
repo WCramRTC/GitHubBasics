5c5837207adfe30ad86f2fd6a2d8f56a3ad7c2bd
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-12-29 11:56:19 -0800

    Added message about myself

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Professor!");
+            Console.WriteLine("My name is William Cram, and I like to build games");
         }
     }
 }
